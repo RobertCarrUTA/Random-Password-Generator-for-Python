df65fc62383e936cdc7750af9a2417177df3971b
Robert Carr <robertcarrcisd@gmail.com>
2022-08-14 21:52:02 -0500

    Added more password generation options
    The passwords can now contain digits, punctuation, and special characters as well as uppercase or lowercase letters.

--- passwordGenerate.py
+++ passwordGenerate.py
@@ -1,15 +1,41 @@
 import random
 import string
+import sys
 
-print('0. Lowercase letters')
+list = """
+0. Lowercase letters
+1. Uppercase letters
+2. Lowercase and Uppercase letters
+3. Digits
+4. Special Symbols (!”#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
+5. Generate Password using letters, digits, and special characters
+"""
 
-print('Please select from the above list the character types of your password: ')
+print(list)
+print('Please select from the above list the character types of your password:')
 
 choice = int(input())
 
-print('Please enter a password length in the range of 8-256')
+print('Please enter a password length in the range of 8-256:')
 
 length = int(input())
 
+# Perform a sys.exit() if the length is outside of the accepted boundaries
+if (length < 8 or length > 256):
+    print('Error: The length must be between the values of 8-256. The program will now exit.')
+    sys.exit()
+
+print('\nYour password is:')
+
 if (choice == 0):
     print(''.join(random.choices(string.ascii_lowercase, k = length)))
+elif (choice == 1):
+    print(''.join(random.choices(string.ascii_uppercase, k = length)))
+elif (choice == 2):
+    print(''.join(random.choices(string.ascii_letters, k = length)))
+elif (choice == 3):
+    print(''.join(random.choices(string.digits, k = length)))
+elif (choice == 4):
+    print(''.join(random.choices(string.punctuation, k = length)))
+elif (choice == 5):
+    print(''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k = length)))
